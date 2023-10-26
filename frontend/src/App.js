afe4d187b61baedbf636e4b9f9db5a6a255632f8
nathancurameng <nathan.curameng@gmail.com>
2023-10-26 15:05:15 -0400

    Removed methods that were no longer needed and changed import to StartPage instead of MainPage

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,29 +1,15 @@
 import axios from 'axios';
 import './App.css';
 import React from 'react';
-import MainPage from './components/MainPage';
+import StartPage from './components/StartPage';
 import Scoreboard from './components/Scoreboard';
 
 
 class App extends React.Component {
-  state = {details : [], }
-
-  componentDidMount(){
-    let data;
-    axios.get("http://127.0.0.1:8000/")
-    .then(res => {
-      data = res.data;
-      this.setState({
-        details: data
-      })
-    })
-    .catch(err => { })
-  }
-
   render(){
     return(
       <div>
-        <MainPage />
+        <StartPage />
         <Scoreboard />
       </div>
       

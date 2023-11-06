eeffebc898a69fc81cbab7672997f2f628a007e2
nathancurameng <nathan.curameng@gmail.com>
2023-11-06 12:14:35 -0500

    reformatted some files

--- frontend/src/components/Cookies.jsx
+++ frontend/src/components/Cookies.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { useState, useEffect} from 'react'
+import { useState, useEffect } from 'react'
 import "./styles/cookie-styles.css"
 import "./styles/upgrade-styles.css"
 import peach from "../assets/peach.png"
@@ -15,20 +15,21 @@ import elephant from "../assets/maxElephant.gif"
 
 
 
-const Cookies = () => {
+const Cookies = (props) => {
 
   const [cookiesPerSecond, setCookiesPerSecond] = useState(0)
   const [cookieCount, setCookieCount] = useState(100)
-  const [cookiePerClick, setCookiePerClick] = useState (1)
+  const [cookiePerClick, setCookiePerClick] = useState(1)
 
-//ShadowGarden States
+  //ShadowGarden States
   const [numShadows, setNumShadows] = useState(0)
   const [costClicker, setCostClicker] = useState(100)
+  const [selectedShadow, setSelectedShadow] = useState("")
   // const [shadowList, setShadowList] = useState([])
 
   const [openShop, setOpenShop] = useState(false)
 
-//Click upgrade States
+  //Click upgrade States
   const [numClickUpgrade, setNumClickUpgrade] = useState(1)
   const [costClickUpgrade, setCostClickUpgrade] = useState(150)
 
@@ -38,7 +39,7 @@ const Cookies = () => {
     return () => {
       clearInterval(interval);
     };
-  }, );
+  },);
 
   const clickCookiePerSecond = () => {
     setCookieCount(n => n + cookiesPerSecond)
@@ -49,26 +50,26 @@ const Cookies = () => {
   }
 
   const buyClickUpgrade = () => {
-    if(cookieCount >= costClickUpgrade){
+    if (cookieCount >= costClickUpgrade) {
       setNumClickUpgrade(n => n + 1)
       setCookiePerClick(numClickUpgrade => numClickUpgrade + 1)
       setCostClickUpgrade(Math.floor(costClickUpgrade * 1.17))
       setCookieCount(cookieCount - costClickUpgrade)
     }
-    else{
+    else {
       alert("not enough cookies")
     }
   }
-  
+
   const buyClicker = () => {
-    if(cookieCount >= costClicker){
+    if (cookieCount >= costClicker) {
       setNumShadows(numShadows => numShadows + 1)
       setCostClicker(Math.floor(costClicker * 1.17))
       setCookieCount(cookieCount => cookieCount - costClicker)
       setCookiesPerSecond(cookiesPerSecond => cookiesPerSecond + 1)
       setOpenShop(true)
     }
-    else{
+    else {
       alert("not enough cookies")
     }
   }
@@ -77,101 +78,104 @@ const Cookies = () => {
     <div>
       <h1> Cookies per click : {cookiePerClick} </h1>
       <h1> Cookies per second : {cookiesPerSecond} </h1>
-        <div className="cookie_container">
-            <img id = "cookieID" src= { peach } alt="cookie" onClick={() => clickCookie()}width="400" height="400"></img>
-            <h1> Cookies: {Math.floor(cookieCount)} </h1>
+      <h1> Selected Shadow {selectedShadow} </h1>
 
-            <div className="total-upgrade-container">
+      <div className="cookie_container">
+        <img id="cookieID" src={peach} alt="cookie" onClick={() => clickCookie()} width="400" height="400"></img>
+        <h1> Cookies: {Math.floor(cookieCount)} </h1>
 
-              {/* SHADOW GARDEN*/}
-              <div className="upgrade-container">
-                <div className="upgrade-button">
-                  <div className="clicker">
-                    <img id = "clickerID" src={ clicker } alt="clicker" onClick={() => buyClicker()}></img>
-                    <h1> Clickers: {numShadows}</h1>
-                    <h1> Price: {costClicker}</h1>
-        
-                    <Popup trigger = {openShop} setTrigger ={setOpenShop}>
-                      <h1> Chimera Shadow Garden </h1>
-                    </Popup>
+        <div className="total-upgrade-container">
 
-                  </div>
-                </div>
-                <div className="upgrade-zone">
-                  <h1> Jesse's Shadow Garden </h1>
-                    <img id = "divineDogs" src={ divineDogs } alt="divineDogs"></img>
-                  <img id = "rabbit" src={ rabbit } alt="rabbit"></img>
-                  <img id = "rabbit" src={ elephant } alt="rabbit"></img>
-
-                </div>
-                  
-
-                  
-
-                </div>
-              {/* SHADOW GARDEN END*/}
+          {/* SHADOW GARDEN*/}
+          <div className="upgrade-container">
+            <div className="upgrade-button">
+              <div className="clicker">
+                <img id="clickerID" src={clicker} alt="clicker" onClick={() => buyClicker()}></img>
+                <h1> Clickers: {numShadows}</h1>
+                <h1> Price: {costClicker}</h1>
 
 
+                <Popup trigger={openShop} setTrigger={setOpenShop} returnShadow={setSelectedShadow}>
+                  <h1> Chimera Shadow Garden </h1>
+                </Popup>
 
-              {/* TESLA SHOP END*/}
-              <div className="upgrade-container">
-                <div className="upgrade-button-tesla">
-                  <div className = "clicker-upgrade">
-                    <img id = "teslaUpgradeID" src={ tesla } alt="clickUpgrade" onClick={() => buyClickUpgrade()}></img>
-                    <h1> click Upgrade {numClickUpgrade}</h1>
-                    <h1> Price: {costClickUpgrade}</h1>
-                  </div>
-                </div>
-                <div className="upgrade-zone">
-                  <div className="click-upgrade">
-                    <h1> Tsak's Tesla Shop </h1>
-                    <img id = "tesla" src={ tesla } alt="tesla" width="200" height="150"></img>
-                  </div>
-                </div>
               </div>
-              {/* TESLA SHOP END*/}
-
-              {/* DINER SHOP END*/}
-              <div className="upgrade-container">
-                <div className="upgrade-button">
-                  <div className = "clicker-upgrade">
-                    <img id = "clickUpgradeID" src={ clicker } alt="clickUpgrade" onClick={() => buyClickUpgrade()}></img>
-                    <h1> click Upgrade {numClickUpgrade}</h1>
-                    <h1> Price: {costClickUpgrade}</h1>
-                  </div>
-                </div>
-                <div className="upgrade-zone">
-                  <div className="click-upgrade">
-                    <div className="dansDiner">
-                      <h1> Dan's Diner </h1>
-                      <img id = "food"  alt="food" width="200" height="150"></img>
-                    </div>
-                  </div>
-                </div>
-              </div>
-              {/* DINER SHOP END*/}
-
-              {/* MEDBAY SHOP END*/}
-              <div className="upgrade-container">
-                <div className="upgrade-button">
-                  <div className = "clicker-upgrade">
-                    <img id = "clickUpgradeID" src={ clicker } alt="clickUpgrade" onClick={() => buyClickUpgrade()}></img>
-                    <h1> click Upgrade {numClickUpgrade}</h1>
-                    <h1> Price: {costClickUpgrade}</h1>
-                  </div>
-                </div>
-                <div className="upgrade-zone">
-                  <div className="click-upgrade">
-                    <h1> Bricker's Medbay </h1>
-                    <img id = "medbay"  alt="medbay" width="200" height="150"></img>
-                  </div>
-                </div>
-              </div>
-              {/* MEDBAY SHOP END*/}
-
+            </div>
+            <div className="upgrade-zone">
+              <h1> Jesse's Shadow Garden </h1>
+              <img id="divineDogs" src={divineDogs} alt="divineDogs"></img>
+              <img id="rabbit" src={rabbit} alt="rabbit"></img>
+              <img id="rabbit" src={elephant} alt="rabbit"></img>
 
             </div>
+
+
+
+
+          </div>
+          {/* SHADOW GARDEN END*/}
+
+
+
+          {/* TESLA SHOP END*/}
+          <div className="upgrade-container">
+            <div className="upgrade-button-tesla">
+              <div className="clicker-upgrade">
+                <img id="teslaUpgradeID" src={tesla} alt="clickUpgrade" onClick={() => buyClickUpgrade()}></img>
+                <h1> click Upgrade {numClickUpgrade}</h1>
+                <h1> Price: {costClickUpgrade}</h1>
+              </div>
+            </div>
+            <div className="upgrade-zone">
+              <div className="click-upgrade">
+                <h1> Tsak's Tesla Shop </h1>
+                <img id="tesla" src={tesla} alt="tesla" width="200" height="150"></img>
+              </div>
+            </div>
+          </div>
+          {/* TESLA SHOP END*/}
+
+          {/* DINER SHOP END*/}
+          <div className="upgrade-container">
+            <div className="upgrade-button">
+              <div className="clicker-upgrade">
+                <img id="clickUpgradeID" src={clicker} alt="clickUpgrade" onClick={() => buyClickUpgrade()}></img>
+                <h1> click Upgrade {numClickUpgrade}</h1>
+                <h1> Price: {costClickUpgrade}</h1>
+              </div>
+            </div>
+            <div className="upgrade-zone">
+              <div className="click-upgrade">
+                <div className="dansDiner">
+                  <h1> Dan's Diner </h1>
+                  <img id="food" alt="food" width="200" height="150"></img>
+                </div>
+              </div>
+            </div>
+          </div>
+          {/* DINER SHOP END*/}
+
+          {/* MEDBAY SHOP END*/}
+          <div className="upgrade-container">
+            <div className="upgrade-button">
+              <div className="clicker-upgrade">
+                <img id="clickUpgradeID" src={clicker} alt="clickUpgrade" onClick={() => buyClickUpgrade()}></img>
+                <h1> click Upgrade {numClickUpgrade}</h1>
+                <h1> Price: {costClickUpgrade}</h1>
+              </div>
+            </div>
+            <div className="upgrade-zone">
+              <div className="click-upgrade">
+                <h1> Bricker's Medbay </h1>
+                <img id="medbay" alt="medbay" width="200" height="150"></img>
+              </div>
+            </div>
+          </div>
+          {/* MEDBAY SHOP END*/}
+
+
         </div>
+      </div>
     </div>
   )
 }
--- frontend/src/components/ShopPopup.jsx
+++ frontend/src/components/ShopPopup.jsx
@@ -11,37 +11,41 @@ const ShopPopup = (props) => {
 
     //make a string thats empty and when you click a button it changes the string to the name oft he titem and returns that so that 
 
-    
+    function returnShadow() {
+        return (selectedShadow)
+    }
+
+
     var [selectedShadow, setSelectedShadow] = useState("")
 
-  return (props.trigger) ? (
-    <div className = 'popup'>
-        <div className="popup-container">
-            <button className='close-btn' onClick ={() => props.setTrigger(false)}> Leave </button>
-            { props.children }
+    return (props.trigger) ? (
+        <div className='popup'>
+            <div className="popup-container">
+                <button className='close-btn' onClick={() => props.setTrigger(false)}> Leave </button>
+                {props.children}
 
 
-            <div className="shopOptions">
-                <div className="item1">
-                <img id = "divineDogs" src= { divineDogs } onClick={() => {setSelectedShadow("divineDogs"); props.setTrigger(false)}}alt="divineDogs"></img>
-                    <h2> Divine Dogs </h2>
+                <div className="shopOptions">
+                    <div className="item1">
+                        <img id="divineDogs" src={divineDogs} onClick={() => { setSelectedShadow("divineDogs"); props.setTrigger(false) }} alt="divineDogs"></img>
+                        <h2> Divine Dogs </h2>
+                    </div>
+                    <div className="item2">
+                        <img id="rabbit" src={rabbit} onClick={() => setSelectedShadow("rabbitEscape")} alt="rabbit"></img>
+                        <h2> Rabbit Escape </h2>
+                    </div>
+                    <div className="item3">
+                        <img id="maxElephant" src={maxElephant} onClick={() => setSelectedShadow("maxElephant")} alt="maxElephant"></img>
+                        <h2> Max Elephant </h2>
+                    </div>
                 </div>
-                <div className="item2">
-                    <img id = "rabbit" src= { rabbit } onClick={() => setSelectedShadow("rabbitEscape")} alt="rabbit"></img>
-                    <h2> Rabbit Escape </h2>
-                </div>
-                <div className="item3">
-                    <img id = "maxElephant" src= { maxElephant } onClick={() => setSelectedShadow("maxElephant")} alt="maxElephant"></img>
-                    <h2> Max Elephant </h2>
-                </div>
+
+                <h1> Selected Shadow: {selectedShadow} </h1>
+
+
             </div>
-
-            <h1> Selected Shadow: {selectedShadow} </h1>
-
-
         </div>
-    </div>
-  ) : "";
+    ) : "";
 }
 
 export default ShopPopup
